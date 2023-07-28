425446cea623f30dfe1ddcd74f01355768319f41
unknown <kingwebbe@gmail.com>
2023-07-27 22:48:33 -0400

    fixed triangle need to add read me

--- Assets/triangle.js
+++ Assets/triangle.js
@@ -1,11 +1,11 @@
 const shape = require('./Shape')
 class Triangle extends shape {
     constructor(){
-        super('triangle')
+        super('Triangle')
     }
      //render method for triangle (svg syntax for circle shape )
      render(shapeColor){
-        return  `<polygon points ="100, 20 70, 80 130, 80" fill="${shapeColor}" />`
+        return  `<polygon points ="150, 30 105, 120 195, 120" fill="${shapeColor}" />`
         //placeholder svg for triangle
       }
 }
